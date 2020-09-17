39e2c2e47c5ae2cc0ac7197295997f722171f5b1
Dennis Mathew <dennismacneduvelil@gmail.com>
2020-09-17 14:09:04 +0530

    Updates

---
+++ precache-manifest.0cf70d31ec5bcf1625c72d138efceb96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a277bbdad77f0a8050f6f08d90a2b21",
+    "url": "/React-TodoApp/index.html"
+  },
+  {
+    "revision": "4856c5ba58a9c2fee6ae",
+    "url": "/React-TodoApp/static/css/main.6322af92.chunk.css"
+  },
+  {
+    "revision": "422892d97b8d9185ffea",
+    "url": "/React-TodoApp/static/js/2.4a627c95.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-TodoApp/static/js/2.4a627c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4856c5ba58a9c2fee6ae",
+    "url": "/React-TodoApp/static/js/main.40ded436.chunk.js"
+  },
+  {
+    "revision": "4a0dd028ee9add505228",
+    "url": "/React-TodoApp/static/js/runtime-main.1ca20da4.js"
+  }
+]);
